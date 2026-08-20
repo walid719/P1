d4794ae8f7c8004cb074e40e2832807dc7da5496
walid00 <walid0w0amer@gmail.com>
2026-08-20 17:49:36 +0300

    Add NEW Updating to My project

--- MyFirstProject.cpp
+++ MyFirstProject.cpp
@@ -10,6 +10,13 @@
 
 using namespace std;
 
+enum enOperation
+{
+	Add = 1,
+	Subtract = 2,
+	Multiply = 3,
+	Divide = 4
+};
 
 int ReadNumber(string Message)
 {
@@ -18,27 +25,73 @@ int ReadNumber(string Message)
 	cin>> number;
 	return number;
 }
+
+int ReadOperation()
+{
+	int operation;
+	cout << "\nChoose the operation you want to perform: " << endl;
+	cout << "1. Add" << endl;
+	cout << "2. Subtract" << endl;
+	cout << "3. Multiply" << endl;
+	cout << "4. Divide" << endl;	
+	cout << "Enter your choice (1-4): ";
+	cout << "\n-------------------------------------\n => ";
+	cin >> operation;
+	return operation;
+}
+
 int AddNumbers(int a, int b)
 {
 	int sum = 0;
 	sum = a + b;
 	return sum;
 }
-void PrintSumResult(int sum, int a, int b)
+
+int SubtractNumbers(int a, int b)
 {
-	cout << "\nThe sum of " << a << " and " << b << " is : " << sum << endl;
+	int difference = 0;
+	difference = a - b;
+	return difference;
+}
+
+string PrintSResultOfOperation(int result, int a, int b ,string operationName)
+{
+	return "\nThe Result of " + to_string(a) + " " + operationName + " " + to_string(b) + " is : " + to_string(result) + "\n";
+}
+void PrintResult( int a,int b, enOperation operation)
+{
+	switch (operation)
+	{
+	case enOperation::Add:
+		cout << PrintSResultOfOperation(AddNumbers(a,b),a,b,"+");
+		break;
+	case enOperation::Subtract:
+		cout << PrintSResultOfOperation(SubtractNumbers(a, b), a, b, "-");
+		break;
+	default:
+		cout << "Invalid operation!" << endl;
+	}
 }
 
 int main()
 {
-	int *Number1, *Number2;
+	int* Number1, * Number2;
+	enOperation *Operation;
 	Number1 = new int;
 	Number2 = new int;
+	Operation = new enOperation;
+
 	*Number1 = ReadNumber("Enter first number1 => ");
 	*Number2 = ReadNumber("Enter second number2 => ");
-	PrintSumResult(AddNumbers(*Number1, *Number2), *Number1, *Number2);
+	*Operation = (enOperation)ReadOperation();
+
+	PrintResult(*Number1, *Number2, *Operation);
+
 	delete Number1;
 	delete Number2;
+	delete Operation;
+
+	system("pause>0");	
 	return 0;
 }
 
