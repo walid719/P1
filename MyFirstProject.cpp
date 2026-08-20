1d4571318637c067f3059967dad67ce14719ef91
walid00 <walid0w0amer@gmail.com>
2026-08-20 16:48:55 +0300

    Update in My project

--- MyFirstProject.cpp
+++ MyFirstProject.cpp
@@ -24,14 +24,21 @@ int AddNumbers(int a, int b)
 	sum = a + b;
 	return sum;
 }
+void PrintSumResult(int sum, int a, int b)
+{
+	cout << "\nThe sum of " << a << " and " << b << " is : " << sum << endl;
+}
 
 int main()
 {
-    
-	int Number1 = ReadNumber("Enter first number1 => ");
-	int Number2 = ReadNumber("Enter second number2 => ");
-	cout << "\nSum of two numbers : "<<AddNumbers(Number1, Number2)	 << endl;
-	
+	int *Number1, *Number2;
+	Number1 = new int;
+	Number2 = new int;
+	*Number1 = ReadNumber("Enter first number1 => ");
+	*Number2 = ReadNumber("Enter second number2 => ");
+	PrintSumResult(AddNumbers(*Number1, *Number2), *Number1, *Number2);
+	delete Number1;
+	delete Number2;
 	return 0;
 }
 
